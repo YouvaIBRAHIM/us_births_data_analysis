1f3da826dc108a18d34457281adef8719618de0b
YouvaIBRAHIM <youva.ibrahim@gmail.com>
2024-09-01 22:19:37 +0200

    feat: download table stats in csv format

--- front/src/components/StatisticsView/StatsListView.tsx
+++ front/src/components/StatisticsView/StatsListView.tsx
@@ -6,15 +6,56 @@ import TableContainer from "@mui/material/TableContainer"
 import StatsTableHead from "./StatsTableHead";
 import StatsTableBody from "./StatsTableBody";
 import { useFormStore } from "./form/Form.store";
+import { Button, TableCell, TableHead, TableRow } from "@mui/material";
+import { FileCsv } from "@phosphor-icons/react";
 
 const StatsListView = () => {
-  const { result } = useFormStore()
+  const { result, form } = useFormStore()
+
+  const downloadCsvFile = () => {
+    let csvContent = "data:text/csv;charset=utf-8,";
+
+    if (result?.cells && result.rows) {
+      let cells = result.cells.join(",");
+      csvContent += cells + "\r\n";
+
+      result.rows.forEach(function(rowArray) {
+          let row = rowArray.join(",");
+          csvContent += row + "\r\n";
+      });
+    }
+
+    const encodedUri = encodeURI(csvContent);
+    const link = document.createElement("a");
+    link.setAttribute("href", encodedUri);
+    link.setAttribute("download", `${form.title.replaceAll(' ', '_')}.csv`);
+    document.body.appendChild(link);
+
+    link.click();
+  }
   return (
     <Box sx={{ width: "100%" }}>
       {
         result && 
         <TableContainer component={Paper}>
-          <Table sx={{ minWidth: 700 }} aria-label="customized table">
+          <Table aria-label="Stats table">
+            <TableHead>
+              <TableRow>
+                <TableCell 
+                  size="small" 
+                  colSpan={result.cells.length}
+                  align="right"
+                >
+                  <Button 
+                    onClick={() => downloadCsvFile()}
+                    variant="contained" 
+                    endIcon={<FileCsv size={24} />}
+                  >
+                    Télécharger
+                  </Button>
+                </TableCell>
+              </TableRow>
+            </TableHead>
             <StatsTableHead headCells={result.cells}/>
             <StatsTableBody rows={result.rows}/>
           </Table>
